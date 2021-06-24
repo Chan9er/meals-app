19d78cf4423c429daf9f5cf2015996668e904bf5
lincj <lincj5409@gmail.com>
2021-06-24 17:23:44 +0900

    Fix issues.

--- components/HeaderButtons.js
+++ components/HeaderButtons.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import {} from 'react-native';
-import {HeaderButton} from 'react-navigation-header-buttons';
+import { HeaderButton } from 'react-navigation-header-buttons';
 import { Ionicons } from '@expo/vector-icons';
 import COLOR from '../constant/Colors';
 import { Platform } from 'react-native';
 
-
-
-const CustomHeaderButton = props => {
-    return (
-        <HeaderButton {...props} 
-        IconComponent={Ionicons}
-        iconSize={23}
-        color={Platform.OS === 'android' ? 'white' : Colors.primaryColor}/>
-    );
-}
+const CustomHeaderButton = (props) => {
+  return (
+    <HeaderButton
+      {...props}
+      IconComponent={Ionicons}
+      iconSize={23}
+      color={Platform.OS === 'android' ? 'white' : COLOR.primaryColor}
+    />
+  );
+};
 
 export default CustomHeaderButton;
-
-
